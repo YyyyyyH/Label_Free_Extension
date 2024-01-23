ab6a9ebdeaf1a8b229b6ad31a0ea58bfa5684550
YyyyyyH <149113022+YyyyyyH@users.noreply.github.com>
2024-01-23 16:39:36 +0000

    Update dsprites.py

--- src/dsprites.py
+++ src/dsprites.py
@@ -13,10 +13,10 @@ from captum.attr import GradientShap
 from utils.features import attribute_individual_dim
 from torch.utils.data import random_split
 
-from disentangeling.vae import VAE
-from disentangeling.encoder import EncoderBurgess
-from disentangeling.decoder import DecoderBurgess
-from disentangeling.losses import BetaHLoss, BtcvaeLoss
+from disentangling.vae import VAE
+from disentangling.encoder import EncoderBurgess
+from disentangling.decoder import DecoderBurgess
+from disentangling.losses import BetaHLoss, BtcvaeLoss
 from utils.datasets import DSprites
 from utils.metrics import (
     compute_metrics,
