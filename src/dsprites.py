c54c9866a8207e8afa0ab311e199f1db2c92efa9
YyyyyyH <149113022+YyyyyyH@users.noreply.github.com>
2024-01-23 17:37:04 +0000

    Update dsprites.py

--- src/dsprites.py
+++ src/dsprites.py
@@ -64,7 +64,7 @@ def disvae_feature_importance(
         os.makedirs(save_dir)
 
     # Define the computed metrics and create a csv file with appropriate headers
-    loss_list = [BetaHLoss(), BtcvaeLoss(is_mss=False, n_data=len(train_dataset))]
+    loss_list = [FactorKLoss(device=device), BetaHLoss(), BtcvaeLoss(is_mss=False, n_data=len(train_dataset))]
     metric_list = [
         pearson_saliency,
         spearman_saliency,
