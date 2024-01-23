811d806984c54d37664a67877c32c32e57b0e475
YyyyyyH <149113022+YyyyyyH@users.noreply.github.com>
2024-01-23 16:44:08 +0000

    Update dsprites.py

--- src/dsprites.py
+++ src/dsprites.py
@@ -14,7 +14,7 @@ from utils.features import attribute_individual_dim
 from torch.utils.data import random_split
 
 from disentangling.vae import VAE
-from disentangling.encoder import EncoderBurgess
+from disentangling.encoders import EncoderBurgess
 from disentangling.decoder import DecoderBurgess
 from disentangling.losses import BetaHLoss, BtcvaeLoss
 from utils.datasets import DSprites
