927297f42de36942251d9ebfc7ae17b1a06791d6
YyyyyyH <149113022+YyyyyyH@users.noreply.github.com>
2024-01-25 15:52:55 +0000

    Update decoders.py

--- src/disentangling/decoders.py
+++ src/disentangling/decoders.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-from residual import ResidualStack
+from disentangling.residual import ResidualStack
 
 
 # ALL decoders should be called Decoder<Model>
