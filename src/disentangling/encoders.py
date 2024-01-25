a15f0120b5bd067c0c98968cf9ba277f044bd4be
YyyyyyH <149113022+YyyyyyH@users.noreply.github.com>
2024-01-25 15:51:37 +0000

    Update encoders.py

--- src/disentangling/encoders.py
+++ src/disentangling/encoders.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-from models.residual import ResidualStack
+from residual import ResidualStack
 
 
 # ALL encoders should be called Enccoder<Model>
