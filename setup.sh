ef33bb6f3b45ab4c2ff2eacbb856ec92e1cc56e2
YyyyyyH <149113022+YyyyyyH@users.noreply.github.com>
2024-01-26 14:20:04 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# 创建新的 Conda 环境
-conda create -n LFE python=3.8 -y
+conda env create -f environment.yml
 conda activate LFE
 
-# 安装 Conda 包
-conda install numpy pandas matplotlib scipy tqdm wget pillow seaborn -y
-conda install -c pytorch torch torchvision -y
-
-# 安装 pip 包
-pip install captum medmnist
-
-echo "环境设置完成。"
+conda install pytorch torchvision -c pytorch
