bc51b46ed53ff55053ae168eeb0abbb6977860de
YyyyyyH <149113022+YyyyyyH@users.noreply.github.com>
2024-01-26 17:24:49 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ source ~/anaconda3/etc/profile.d/conda.sh
 
 # Create the LFE environment with specified dependencies
 conda create -n LFE python=3.8 numpy pandas matplotlib scipy tqdm pillow torch seaborn pathlib wget -y -c pytorch -c defaults
+# conda create -n LFE python=3.8 numpy pandas matplotlib scipy tqdm pillow seaborn pathlib wget pytorch torchvision -c pytorch -c defaults -y
 conda activate LFE
 
 # Install PyTorch and torchvision
