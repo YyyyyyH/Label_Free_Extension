65b79cdc56397d2d9daa7c604f128f3cf858f719
YyyyyyH <149113022+YyyyyyH@users.noreply.github.com>
2024-01-26 17:08:32 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+source ~/anaconda3/etc/profile.d/conda.sh
+
+# 创建并激活环境
 conda env create -f environment.yml
 conda activate LFE
 
+# 安装其他依赖
 conda install pytorch torchvision -c pytorch
 
